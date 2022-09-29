1352472eeaf95d383c108390be964f9fa6635ea0
EduardDevBD <lorenzoeduard789@gmail.com>
2022-09-29 14:23:35 -0500

    Agregando la carpeta de config

---
+++ config/conexion.php
@@ -0,0 +1,18 @@
+<?php
+class Conectar{
+    protected $dbh;
+    protected function Conexion(){
+        try {
+            $conectar=$this->dbh = new PDO("mysql:host=localhost;dbname=paginawebphp", "root", "");
+            return $conectar;
+        } catch (Exception $e) {
+            print "Error: ".$e->getMessage();
+            die();
+        }
+    }
+
+    public function set_names() {
+        return $this->dbh->query("SET NAMES 'utf8'");
+    }   
+}
+?>  
