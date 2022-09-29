7b08943643f5e6f9a1cbe011d89538660ec6079e
EduardDevBD <lorenzoeduard789@gmail.com>
2022-09-29 14:53:23 -0500

    29/09/2022 Produccion Heroku

--- config/conexion.php
+++ config/conexion.php
@@ -3,7 +3,10 @@ class Conectar{
     protected $dbh;
     protected function Conexion(){
         try {
-            $conectar=$this->dbh = new PDO("mysql:host=localhost;dbname=paginawebphp", "root", "");
+            // TODO: Conexion a base de datos local
+            //$conectar=$this->dbh = new PDO("mysql:host=localhost;dbname=paginawebphp", "root", "");
+            // TODO: Conexion a base de datos HEROKU
+            $conectar=$this->dbh = new PDO("mysql:host=us-cdbr-east-06.cleardb.net;dbname=heroku_646cd8936eb6efd", "b560f920f19b0e", "5cc9f806");
             return $conectar;
         } catch (Exception $e) {
             print "Error: ".$e->getMessage();
